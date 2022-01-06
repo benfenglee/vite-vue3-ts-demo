4c46ebab20d7227285ba7b224020c5fcb2509804
benfenglee <benfenglee@gmail.com>
2022-01-06 23:25:19 +0800

    首页清空

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,23 +1,8 @@
 <script lang="ts" setup>
-import {useStore} from 'vuex'
-import {computed} from 'vue'
-const store = useStore()
-const add = () =>store.commit('SET_COUNT')
-const count = computed(()=> store.state.count)
-import HelloWorldVue from '../../components/HelloWorld.vue';
-import CMenu from '../../components/CMenu.vue';
-import Layout from '../../layout/layout.vue';
-const time = new Date().format('yyyy-MM-dd')
+
 </script>
-<template>
-woshi
-</template>
+<template>首页</template>
 
-
-<style>
-.item {
-}
-</style>
 <style lang="" scoped>
   
 </style>
