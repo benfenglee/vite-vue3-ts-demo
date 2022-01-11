e70f3584f9c3e57f4dbf138831fd61da3540d8d5
benfenglee <benfenglee@gmail.com>
2022-01-11 18:09:48 +0800

    新增iconfont

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import vuex_storage from './utils/vuex_storage'
-onMounted(() => vuex_storage())
+import { onMounted } from "vue";
+import vuex_storage from "./utils/vuex_storage";
+onMounted(() => vuex_storage());
 // 选择主题
-window.document.documentElement.setAttribute("data-theme", 'default');
+window.document.documentElement.setAttribute("data-theme", "default");
 // 进行一些初始化设置
-
 </script>
 
 <template>
+  <link
+    rel="stylesheet"
+    href="https://at.alicdn.com/t/font_3133183_jq38ukm9mpm.css?spm=a313x.7781069.1998910419.177&file=font_3133183_jq38ukm9mpm.css"
+  />
   <router-view></router-view>
 </template>
 
